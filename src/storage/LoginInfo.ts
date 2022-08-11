06d3c179b291e9e89d16d7b5a425a79cc3530577
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-08-11 20:46:44 +0900

    More values are stored in sessionstorage

---
+++ src/storage/LoginInfo.ts
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { MissingTokenError } from "../class/error/MissingTokenError";
+import { AccessToken, Homeserver, UserID } from "../types/Types";
+
+export type LoginData = {
+	homeserver: Homeserver;
+    token: AccessToken;
+    uid: UserID;
+}
+
+export function getLoginInfo(): LoginData{
+	const nav = useNavigate();
+	const token = sessionStorage.getItem('token');
+	const homeserver = sessionStorage.getItem('homeserver');
+	const uid = sessionStorage.getItem('uid');
+	if(!token || !homeserver || !uid) {
+		nav('/');
+		throw new MissingTokenError();
+	}
+	return {
+		homeserver: homeserver as Homeserver,
+		uid: uid as UserID,
+		token: token as AccessToken
+	}
+}
+
+export function setToken(data: LoginData){
+	sessionStorage.setItem('token', data.token);
+	sessionStorage.setItem('homeserver', data.homeserver);
+	sessionStorage.setitem('uid', data.uid);
+}
--- src/types/Types.ts
+++ src/types/Types.ts
@@ -1,1 +1,3 @@
-export type AccessToken = string & {'_type': 'AccessToken'};
+export type AccessToken = string & {_type: 'AccessToken'};
+export type Homeserver = string & {_type: 'Homeserver'};
+export type UserID = string & {_type: 'UserID'};
