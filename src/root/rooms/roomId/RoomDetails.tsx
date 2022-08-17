3d198448d004f92495c0ad297f615c63438b0514
MangoCubes <10383115+MangoCubes@users.noreply.github.com>
2022-08-17 23:38:56 +0900

    Moved state loading to room details page

--- src/root/rooms/roomId/RoomDetails.tsx
+++ src/root/rooms/roomId/RoomDetails.tsx
@@ -1,9 +1,12 @@
 import { Skeleton, Typography, Stack, Box, CardContent, CircularProgress, AppBar, Toolbar, Card, Tabs, Tab } from "@mui/material";
-import { useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Room } from "../../../types/Room";
+import handleCommonErrors from "../../../functions/handleCommonErrors";
+import { GetRoomStateQuery } from "../../../query/GetRoomStateQuery";
+import { LoginContext } from "../../../storage/LoginInfo";
+import { Room, RoomState } from "../../../types/Room";
 import { RoomDetailsEdit } from "./RoomDetailsEdit";
 
 enum TabName {
@@ -11,29 +14,69 @@ enum TabName {
 	Members = 'members'
 }
 
+enum LoadState {
+	LoadingRoom,
+	LoadingStates,
+	Done,
+	Invalid
+}
+
+type Loading = {
+	step: LoadState.LoadingRoom;
+} | {
+	step: LoadState.LoadingStates;
+	room: Room;
+} | {
+	step: LoadState.Done;
+	room: Room;
+	states: RoomState[];
+}
+
+type AllLoadingStates = Loading | {step: LoadState.Invalid};
+
 export function RoomDetails(props: {rooms: Room[] | null}){
 
 	const [currentTab, setCurrentTab] = useState<TabName>(TabName.Details);
 	const [disableTabs, setDisableTabs] = useState<boolean>(false);
+	const [currentState, setCurrentState] = useState<AllLoadingStates>({step: LoadState.LoadingRoom});
 
-	const {t} = useTranslation();
+	const {homeserver, token} = useContext(LoginContext);
+
+	const con = useRef<AbortController | null>(null);
 
 	const {rid} = useParams();
 
-	const nav = useNavigate();
-
-	const getTitle = () => {
-		if(props.rooms === null) return <Skeleton sx={{width: '40%'}}/>;
-		const r = props.rooms.find(r => r.room_id === rid);
-		if (!r) {
-			toast.error(t('room.invalidRoom'));
-			nav('../');
-			return false;
-		} else return <Typography variant='h6'>{t('room.title', {name: r.room_id})}</Typography>
+	const getStates = async (room: Room) => {
+		setCurrentState({step: LoadState.LoadingStates, room: room});
+		try{
+			const req = new GetRoomStateQuery(homeserver, {rid: room.room_id}, token);
+			con.current = req.con;
+			const res = await req.send();
+			setCurrentState({states: res.state, room: room, step: LoadState.Done});
+		} catch (e) {
+			if (e instanceof Error) handleCommonErrors(e, t);
+		}
 	}
 
-	const getRoomDetails = () => {
-		if(props.rooms === null) return (
+	useEffect(() => {
+		if(props.rooms){
+			const r = props.rooms.find(r => r.room_id === rid);
+			if(!r) setCurrentState({step: LoadState.Invalid});
+			else if (currentState.step === LoadState.LoadingRoom) getStates(r);
+		}
+	}, [currentState, props.rooms]);
+
+	const {t} = useTranslation();
+
+	const nav = useNavigate();
+
+	const getTitle = (l: Loading) => {
+		if(l.step === LoadState.LoadingRoom) return <Skeleton sx={{width: '40%'}}/>;
+		else return <Typography variant='h6'>{t('room.title', {name: l.room.room_id})}</Typography>
+	}
+
+	const getRoomDetails = (l: Loading) => {
+		if(l.step === LoadState.LoadingRoom) return (
 			<Stack direction='row' spacing={2}>
 				<Box>
 					<Typography variant='h5' sx={{width: '128px'}}><Skeleton/></Typography>
@@ -42,46 +85,48 @@ export function RoomDetails(props: {rooms: Room[] | null}){
 				</Box>
 			</Stack>
 		)
-		const r = props.rooms.find(r => r.room_id === rid);
-		if (r) {
-			return (
-			<Stack direction='row' spacing={2}>
-				<Box key='name'>
-					{r.name && r.name !== '' ? <Typography variant='h5'>{r.name}</Typography> : <Typography variant='h5' color='GrayText'>{t('room.noName')}</Typography>}
-					{r.canonical_alias && r.canonical_alias !== '' ? <Typography variant='h6'>{r.canonical_alias}</Typography> : <Typography variant='h6' color='GrayText'>{t('room.noAlias')}</Typography>}
-					<Typography variant='caption' color='GrayText'>{r.room_id}</Typography>
-				</Box>
-			</Stack>
-			);
-		}
-		else return false;
+		const r = l.room;
+		return (
+		<Stack direction='row' spacing={2}>
+			<Box key='name'>
+				{r.name && r.name !== '' ? <Typography variant='h5'>{r.name}</Typography> : <Typography variant='h5' color='GrayText'>{t('room.noName')}</Typography>}
+				{r.canonical_alias && r.canonical_alias !== '' ? <Typography variant='h6'>{r.canonical_alias}</Typography> : <Typography variant='h6' color='GrayText'>{t('room.noAlias')}</Typography>}
+				<Typography variant='caption' color='GrayText'>{r.room_id}</Typography>
+			</Box>
+		</Stack>
+		);
 	}
 
-	const getCurrentContent = () => {
-		if(props.rooms === null) return (
+	const getCurrentContent = (l: Loading) => {
+		if(l.step === LoadState.LoadingRoom || l.step === LoadState.LoadingStates) return (
 			<CardContent sx={{flex: 1, display: 'flex', flexDirection: 'column'}}>
 				<Box width='100%' textAlign='center'>
 					<CircularProgress/>
 				</Box>
 			</CardContent>
 		);
-		const r = props.rooms.find(r => r.room_id === rid);
-		if(!r) return false;
-		if(currentTab === TabName.Details) return <RoomDetailsEdit room={r} disableTabs={setDisableTabs}/>;
+		if(currentTab === TabName.Details) return <RoomDetailsEdit room={l.room} states={l.states} disableTabs={setDisableTabs}/>;
+		
 		else return <Box sx={{flex: 1}}></Box>;
 	}
 
-	return (
+	const returnToRooms = () => {
+		toast.error(t('room.invalidRoom'));
+		nav('../');
+	}
+
+	if(currentState.step === LoadState.Invalid) return <>{returnToRooms()}</>;
+	else return (
 	<Box sx={{display: 'flex', flexDirection: 'column', height: '100%'}}>
 		<AppBar position='static'>
 			<Toolbar>
-				{getTitle()}
+				{getTitle(currentState)}
 			</Toolbar>
 		</AppBar>
 		<Box m={2} sx={{flex: 1, display: 'flex', flexDirection: 'column'}}>
 			<Card>
 				<CardContent>
-					{getRoomDetails()}
+					{getRoomDetails(currentState)}
 				</CardContent>
 			</Card>
 			<Card sx={{mt: 2, flex: 1, display: 'flex', flexDirection: 'column'}}>
@@ -90,7 +135,7 @@ export function RoomDetails(props: {rooms: Room[] | null}){
 						<Tab disabled={disableTabs || props.rooms === null} value={TabName.Details} label={t('room.details.title')}/>
 					</Tabs>
 				</Box>
-				{getCurrentContent()}
+				{getCurrentContent(currentState)}
 			</Card>
 		</Box>
 	</Box>
--- src/root/rooms/roomId/RoomDetailsEdit.tsx
+++ src/root/rooms/roomId/RoomDetailsEdit.tsx
@@ -1,25 +1,20 @@
 import { CardContent, CardActions, Button } from "@mui/material";
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import handleCommonErrors from "../../../functions/handleCommonErrors";
 import { DeleteRoomQuery } from "../../../query/DeleteRoomQuery";
-import { GetRoomStateQuery } from "../../../query/GetRoomStateQuery";
 import { LoginContext } from "../../../storage/LoginInfo";
 import { Room, RoomState } from "../../../types/Room";
 
-export function RoomDetailsEdit(props: {room: Room, disableTabs: (to: boolean) => void}) {
+export function RoomDetailsEdit(props: {room: Room, states: RoomState[], disableTabs: (to: boolean) => void}) {
 
 	const {t} = useTranslation();
 
 	const {homeserver, token} = useContext(LoginContext);
 
 	const [querying, setQuerying] = useState<boolean>(false);
-	const [reload, setReload] = useState(true);
-	const [states, setStates] = useState<RoomState[] | null>(null);
-
-	const con = useRef<AbortController | null>(null);
 
 	const nav = useNavigate();
 
@@ -36,29 +31,6 @@ export function RoomDetailsEdit(props: {room: Room, disableTabs: (to: boolean) =
 			setQuerying(false);
 		}
 	}
-
-	const getStates = async () => {
-		setStates(null);
-		setReload(false);
-		try{
-			const req = new GetRoomStateQuery(homeserver, {rid: props.room.room_id}, token);
-			con.current = req.con;
-			const res = await req.send();
-			setStates(res.state);
-		} catch (e) {
-			if (e instanceof Error) handleCommonErrors(e, t);
-		}
-	}
-
-	useEffect(() => {
-		if(reload) getStates();
-	}, [reload]);
-
-	useEffect(() => {
-		return () => {
-			if(con.current) con.current.abort();
-		}
-	}, []);
 
 	useEffect(() => {
 		props.disableTabs(querying);
